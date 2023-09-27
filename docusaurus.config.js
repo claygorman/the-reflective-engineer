c52185a552e6fa2fccae53220450df587a5e862d
claygorman <claygorman@gmail.com>
2023-09-27 15:49:20 -0700

    Updating the docusaurus config for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,23 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'The Reflective Engineer',
+  tagline: 'Reflections on software engineering',
   favicon: 'img/favicon.ico',
 
+  // Recommended for github
+  trailingSlash: true,
+
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://thereflectiveengineer.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'claygorman', // Usually your GitHub org/user name.
+  projectName: 'the-reflective-engineer', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
